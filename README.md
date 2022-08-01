4e3f1bf994b6c4651f2b251180617a934b2ee199
Merve Yavuz <merveyavuz2533@gmail.com>
2022-08-01 22:40:49 +0300

    odev1 yapıldı

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# patikaDevSqlOdevleri
+# Patika Sql Odevleri
+
+[Patika](www.patika.dev)
+
+[PostgreSQL Sample Database](https://www.postgresqltutorial.com/postgresql-getting-started/postgresql-sample-database/)
+
--- odev1.sql
+++ odev1.sql
@@ -9,7 +9,7 @@ WHERE length >60 AND length <75;
 SELECT * FROM  film
 WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99) ;
 
---customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
+-- customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
 SELECT first_name, last_name FROM customer
 WHERE first_name = 'Mary';
 
