250c2f5924e768d4ff1e1a2a6cd62cdc9f310549
Merve Yavuz <merveyavuz2533@gmail.com>
2022-08-02 14:49:41 +0300

    odev3 added

--- odev3.sql
+++ odev3.sql
@@ -14,6 +14,6 @@ SELECT title FROM film
 WHERE title ILIKE '%T%T%T%T%';
 
 --film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve
--- uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+--uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 SELECT * FROM film
 WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
